192c00267f46626812aa1407f19c5e3eff9deff1
Mỹ Nguyễn <nguyen@MacBook-Pro-M1Max-64G-2T.local>
2023-06-28 20:30:24 -0700

    b3: Move data to the ViewModel

--- app/src/main/java/com/nguyen/viewmodel1/GameFragment.kt
+++ app/src/main/java/com/nguyen/viewmodel1/GameFragment.kt
@@ -10,11 +10,6 @@ import com.nguyen.viewmodel1.databinding.FragmentGameBinding
  * Fragment where the game is played, contains the game logic.
  */
 class GameFragment : Fragment(R.layout.fragment_game) {
-
-    private var score = 0
-    private var currentWordCount = 0
-    private var currentScrambledWord = "test"
-
     private val viewModel: GameViewModel by viewModels()
 
     // Binding object instance with access to the views in the game_fragment.xml layout
@@ -41,13 +36,6 @@ class GameFragment : Fragment(R.layout.fragment_game) {
     * Displays the next scrambled word.
     */
     private fun onSubmitWord() {
-        currentScrambledWord = getNextScrambledWord()
-        currentWordCount++
-        score += SCORE_INCREASE
-        binding.wordCount.text = getString(R.string.word_count, currentWordCount, MAX_NO_OF_WORDS)
-        binding.score.text = getString(R.string.score, score)
-        setErrorTextField(false)
-        updateNextWordOnScreen()
     }
 
     /*
@@ -55,11 +43,6 @@ class GameFragment : Fragment(R.layout.fragment_game) {
      * Increases the word count.
      */
     private fun onSkipWord() {
-        currentScrambledWord = getNextScrambledWord()
-        currentWordCount++
-        binding.wordCount.text = getString(R.string.word_count, currentWordCount, MAX_NO_OF_WORDS)
-        setErrorTextField(false)
-        updateNextWordOnScreen()
     }
 
     /*
@@ -104,6 +87,6 @@ class GameFragment : Fragment(R.layout.fragment_game) {
      * Displays the next scrambled word on screen.
      */
     private fun updateNextWordOnScreen() {
-        binding.textViewUnscrambledWord.text = currentScrambledWord
+        binding.textViewUnscrambledWord.text = viewModel.currentScrambledWord
     }
 }
--- app/src/main/java/com/nguyen/viewmodel1/GameViewModel.kt
+++ app/src/main/java/com/nguyen/viewmodel1/GameViewModel.kt
@@ -3,4 +3,9 @@ package com.nguyen.viewmodel1
 import androidx.lifecycle.ViewModel
 
 class GameViewModel: ViewModel() {
+    private var score = 0
+    private var currentWordCount = 0
+    private var _currentScrambledWord = "test"
+    val currentScrambledWord: String
+        get() = _currentScrambledWord
 }
